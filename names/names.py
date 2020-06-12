f77bacd14a6e4b38d7b67e445620e8c62ebf3950
Joey Nevarez <josephnevarez12@gmail.com>
2020-06-12 10:03:50 -0700

    update

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from BST import BSTNode
 
 start_time = time.time()
 
@@ -13,16 +14,22 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+
+BST1 = BSTNode("")
+for n1 in names_1:
+    BST1.insert(n1)
+
+
+for n2 in names_2:
+    if BST1.contains(n2):
+        duplicates.append(n2)
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
-
-# ---------- Stretch Goal -----------
-# Python has built-in tools that allow for a very efficient approach to this problem
-# What's the best time you can accomplish?  Thare are no restrictions on techniques or data
-# structures, but you may not import any additional libraries that you did not write yourself.
